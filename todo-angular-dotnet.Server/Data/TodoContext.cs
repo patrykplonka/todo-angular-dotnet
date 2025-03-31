87dfa0d7bd74e85612c0826fba92a9d17bf00ee9
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-03-31 14:11:51 +0200

    Add context to database;

---
+++ todo-angular-dotnet.Server/Data/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo_angular_dotnet.Server.Models;
+
+namespace todo_angular_dotnet.Server.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
--- todo-angular-dotnet.Server/Program.cs
+++ todo-angular-dotnet.Server/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using todo_angular_dotnet.Server.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 var app = builder.Build();
 
 app.UseDefaultFiles();
