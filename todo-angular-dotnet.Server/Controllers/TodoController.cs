ecbe866429a163e8c4289ab9aa47e2070cae7ea5
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-03-31 14:15:50 +0200

    Add API TodoController.

---
+++ todo-angular-dotnet.Server/Controllers/TodoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using todo_angular_dotnet.Server.Data;
+using todo_angular_dotnet.Server.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    private readonly TodoContext _context;
+
+    public TodoController(TodoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
+        return await _context.TodoItems.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+    {
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetTodoItems), new { id = todoItem.Id }, todoItem);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
+    {
+        if (id != todoItem.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(todoItem).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(int id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        _context.TodoItems.Remove(todoItem);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
