db7c6ff24f9fd4236ef564b7ba73fcfcecd87517
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-03-31 14:06:44 +0200

    Add ToDo model.

---
+++ todo-angular-dotnet.Server/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace todo_angular_dotnet.Server.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
