2e355f72430be1000f2aea746a80b7eeab1bf6b8
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-03-31 14:18:37 +0200

    Add todo service.

---
+++ todo-angular-dotnet.client/src/app/todo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface TodoItem {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  private apiUrl = 'api/todo';
+
+  constructor(private http: HttpClient) { }
+
+  getTodoItems(): Observable<TodoItem[]> {
+    return this.http.get<TodoItem[]>(this.apiUrl);
+  }
+
+  addTodoItem(todoItem: TodoItem): Observable<TodoItem> {
+    return this.http.post<TodoItem>(this.apiUrl, todoItem);
+  }
+
+  updateTodoItem(id: number, todoItem: TodoItem): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, todoItem);
+  }
+
+  deleteTodoItem(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
